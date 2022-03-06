91a2ed3f87b97408720b9ce0781019cf184d5fc0
yh <kuba.kucharski15@gmail.com>
2022-03-06 23:01:36 +0100

    Added zad1, zad2

---
+++ zad1/main.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+
+// Traveling Salesman Problem
+
+struct Graph
+{
+	int** adjList;
+};
+
+struct Graph* createGraph()
+{
+	struct Graph* graph = malloc(sizeof(struct Graph));
+	graph->adjList = malloc(N * sizeof(int*));
+	
+	int i, j;
+	for(i = 0; i < N; i++)
+		graph->adjList[i] = malloc(N * sizeof(int));
+	
+	for(i = 0; i < N; i++)
+		for(j = 0; j < N; j++)
+			graph->adjList[i][j] = 0;
+	
+	return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest, int weight)
+{
+	graph->adjList[src][dest] = weight;
+	graph->adjList[dest][src] = weight;
+}
+
+void printGraph(struct Graph* graph)
+{
+	int i, j;
+	
+	for(i = 0; i < N; i++)
+	{
+		for(j = 0; j < N; j++)
+			printf("%d ", graph->adjList[i][j]);
+		
+		printf("\n");
+	}
+}
+
+int arrayContains(int* array, int value)
+{
+	int i;
+	for(i = 0; i < N; i++)
+		if(array[i] == value) 
+			return 1;
+		
+	return 0;
+}
+
+int arrayMinIndex(int* array, int* visited)
+{
+	int i, min = 1000, minInd = -1;
+	int* cpArray = malloc(N * sizeof(int*));
+	
+	for(i = 0; i < N; i++) cpArray[i] = array[i];
+	
+	for(i = 0; i < N; i++)
+		if(arrayContains(visited, i)) 
+			cpArray[i] = 1000;
+	
+	for(i = 0; i < N; i++)
+		if(cpArray[i] > 0 && cpArray[i] < min)
+		{
+			minInd = i;
+			min = cpArray[i];
+		}
+	
+	free(cpArray);
+	return minInd;
+}
+
+void tsp(struct Graph* graph, int start)
+{
+	printf("Path starting from %d: ", start);
+	
+	int i, curr, tmp, length = 0;
+	int* visited = malloc(N * sizeof(int));
+	for(i = 0; i < N; i++) visited[i] = -1;
+	
+	curr = start;
+	
+	for(i = 0; i < N; i++)
+	{
+		visited[i] = curr;
+		tmp = arrayMinIndex(graph->adjList[curr], visited);
+		if(i < N - 1)
+		{
+			length += graph->adjList[curr][tmp];
+			curr = tmp;
+		}
+	}
+	
+	length += graph->adjList[curr][start];
+
+	for(i = 0; i < N; i++) printf("%d ", visited[i]);
+	printf("%d ; length = %d\n", start, length);
+	
+	free(visited);
+}
+
+int main()
+{
+	struct Graph* graph = createGraph();
+	
+	addEdge(graph,0,1,15);
+	addEdge(graph,0,2,12);
+	addEdge(graph,0,3,6);
+	addEdge(graph,0,4,21);
+	addEdge(graph,1,2,3);
+	addEdge(graph,1,3,10);
+	addEdge(graph,1,4,4);
+	addEdge(graph,2,3,11);
+	addEdge(graph,2,4,26);
+	addEdge(graph,3,4,17);
+		
+	printf("Graph:\n");
+	printGraph(graph);
+
+	int i;
+	for(i = 0; i < N; i++) tsp(graph, i);
+}
---
+++ zad2/zad2/zad2.cpp
@@ -0,0 +1,59 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+double funkcjaTestowa(double x)
+{
+    return 3 * sin(M_PI * x / 5) + sin(M_PI * x);
+}
+
+double randomInRange(double start, double end, double absStart, double absEnd)
+{
+    random_device rd;
+    default_random_engine eng(rd());
+    uniform_real_distribution<double> distr(start, end);
+
+    double x = distr(eng);
+
+    if (x < absStart) x = absStart;
+    if (x > absEnd) x = absEnd;
+
+    return x;
+}
+
+double wyzarzanie(double start, double end, double t0, double n, int epoki, int proby)
+{
+    double x0, x1, f0, f1, prob, probRand;
+
+    x0 = randomInRange(0, 10, 0, 10);
+
+    for (int i = 0; i < epoki; i++)
+    {
+        for (int j = 0; j < proby; j++)
+        {
+            f0 = funkcjaTestowa(x0);
+            x1 = randomInRange(x0 - 2 * t0, x0 + 2 * t0, 0, 10);
+            f1 = funkcjaTestowa(x1);
+
+            if (f1 >= f0) x0 = x1;
+            if (f1 < f0)
+            {
+                prob = exp((f1 - f0) / t0);
+                probRand = randomInRange(0, 1, 0, 1);
+                if (prob > probRand) x0 = x1;
+            }
+        }
+
+        t0 *= n;
+    }
+
+    return x0;
+}
+
+int main()
+{
+    cout << wyzarzanie(0, 10, 1, 0.9, 5, 3);
+}
